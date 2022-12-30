b66bf937b40dba0a6684b2952fadda6ad0d410a6
Tarun Choudhary <tarunchoudhary1805@gmail.com>
2022-12-30 08:25:07 +0530

    Comment model added

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
    root 'posts#index', as: 'home'
-   resources :posts
+   resources :posts do
+      resources :comments
+   end
+   
    get 'about' => 'pages#about'
 end
